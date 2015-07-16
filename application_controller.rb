0883154f6bba2b63512435afdfe32161ea0734c0
hamzanazir72 <hamzanazir72@gmail.com>
2015-07-16 13:39:23 +0000

    done

--- application_controller.rb
+++ application_controller.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 Bundler.require
 
+require_relative 'models/model.rb'
+
 class MyApp < Sinatra::Base
 
   get '/' do
--- config.ru
+++ config.ru
@@ -1,5 +1,7 @@
 require 'sinatra'
 require './application_controller.rb'
 
+
+
 run MyApp
 
